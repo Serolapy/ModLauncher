514584a9236f262fce044a19b4d5491369ef4d83
Serolapy <65182656+Serolapy@users.noreply.github.com>
2021-03-29 21:04:56 +0300

    Update publick.js

--- publick.js
+++ publick.js
@@ -477,10 +477,10 @@ function getUpdateMessage(){
 				returnTime = '';
 			if(heute.getDate() == timeMsg.timeMsg() && heute.getMonth() == timeMsg.getMonth() && heute.getFullYear() == timeMsg.getFullYear()){
 				//сегодняшнее сообщение
-				returnTime = timeMsg.getHours() + ':' timeMsg.getMinutes();
+				returnTime = timeMsg.getHours() + ':' + timeMsg.getMinutes();
 			} else{
 				//иначе
-				returnTime = timeMsg.getDate() + '.' (timeMsg.getMonth() + 1);
+				returnTime = timeMsg.getDate() + '.' + (timeMsg.getMonth() + 1);
 			}
 			messagesHtml += '<td class="MLmessage_msg_time">' + returnTime + '</td></tr>';
 		}
